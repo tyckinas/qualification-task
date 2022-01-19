2598b63c86e53db335c7fd1778d74ac14d2a61f0
tyckinas <klikunastitas@gmail.com>
2022-01-19 17:43:05 +0200

    mui table for fetched data added

--- app/src/pages/ListPage/ListPage.jsx
+++ app/src/pages/ListPage/ListPage.jsx
@@ -1,11 +1,111 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+
+import TablePaginationActions from "./TablePaginationActions";
+
+
+
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 const ListPage = () => {
-    return (
-        <div>
-            list
-        </div>
-    )
-}
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [posts, setPosts] = useState([]);
 
-export default ListPage
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - posts.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => setPosts(data.data));
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableBody>
+          {(rowsPerPage > 0
+            ? posts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : posts
+          ).map((row) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.title}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="right">
+                {row.id}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="right">
+                {row.userId}
+              </TableCell>
+            </TableRow>
+          ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+            <TableRow>
+                <TablePagination 
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={3}
+                count={posts.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                    inputProps: {
+                        'aria-label' : 'rows per page',
+                    },
+                    native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                // Fix later maybe
+                // ActionsComponent={TablePaginationActions}
+                />
+            </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+
+  
+};
+
+export default ListPage;
