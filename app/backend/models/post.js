594ed39c7be414ae2ce655b9a869b1b4f9650f30
tyckinas <klikunastitas@gmail.com>
2022-01-21 15:10:17 +0200

    body-parser

--- app/backend/models/post.js
+++ app/backend/models/post.js
@@ -4,11 +4,10 @@ const PostSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-   
   },
   body: {
     type: String,
-    required: true
+    required: true,
   },
 });
 
--- app/backend/server.js
+++ app/backend/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const postRouter = require("./routes/postRoutes.js");
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose.connect(
   "mongodb+srv://admin:admin@cluster0.nwdyk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   }
 );
 
